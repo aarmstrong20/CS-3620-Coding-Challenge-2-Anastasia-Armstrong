fdbdebd7d58df3a39ca98534baeaf430778740b8
Anastasia Armstrong <aarmstrong1@mail.weber.edu>
2025-05-07 09:36:45 -0600

    Created BMI Calculator with user input

---
+++ BMICalculator.py
@@ -0,0 +1,16 @@
+##Body Mass Indicator
+## BMI = (weight in Kg)/(Height in Meters)^2
+## kg = Weight
+## m = meters
+## bmi = BMI or Body Mass Indicator
+
+
+kg = 0
+m = 0
+kg = float(input("Please enter your Weight in Kg: "))
+
+m = float(input("Please enter your Mass in kg: "))
+
+bmi = kg/(m**m)
+
+print("Your BMI is:", bmi)
